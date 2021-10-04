1e3c04fcee503bbcdf7c830a87420c650ead24cc
Ryan <hokai_999@hotmail.com>
2021-10-04 22:40:20 +0800

    add dog bark

--- dog.py
+++ dog.py
@@ -1,4 +1,10 @@
 class dog:
-    def __init__():
+    def __init__(self):
         self.name = 'lucky'
-        
+
+    def bark(self):
+        print('bark and fuck you!')
+
+if __name__ == '__main__':
+    p = dog()
+    p.bark()
